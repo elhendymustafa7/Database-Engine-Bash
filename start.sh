19c1984730bb72f3631171de984b054dfb781c0a
abdelrahman <abdelrahmanch4e@gmail.com>
2022-12-12 23:41:01 +0200

    start page

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#start page to begin Database Engine using bash
 if ! [[  -e `pwd`/DB ]]
 then
     mkdir -p ./DB
@@ -6,5 +7,22 @@ fi
 export maindir=$PWD
 cd ./DB/
 
+clear
+echo -e "\e[96m*************************************************************"
+echo "*                                                           *"
+echo "*                                                           *"
+echo "*          Welcome to Database Engine                       *"
+echo "*                                                           *"
+echo "*      Abdelrahman Anany         Mustafa Elhendy            *"
+echo "*                                                           *"
+echo "*                                                           *"
+echo "*                                                           *"
+echo "*************************************************************"
+
+echo -e "press Enter to continue\e[0m"
+read 
+
+
 . $maindir/homeselect.sh
+
 
